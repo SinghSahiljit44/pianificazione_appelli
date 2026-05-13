fb55ded4a5b47800205bd732e8414183339e2eb8
Matteo Lublanis <matteo.lub02@gmail.com>
2026-05-13 20:57:52 +0200

    Aggiunti esempi su updateappellodto

--- libs/server/academic/appello/src/lib/dto/update-appello.dto.ts
+++ libs/server/academic/appello/src/lib/dto/update-appello.dto.ts
@@ -1,5 +1,6 @@
 import { IsString, IsOptional, IsInt, IsDate } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 const toValidDate = ({ value }: { value: unknown }) => {
   if (typeof value !== 'string') return value;
@@ -10,27 +11,33 @@ const toValidDate = ({ value }: { value: unknown }) => {
 };
 
 export class UpdateAppelloDto {
+    @ApiPropertyOptional({ example: '2025-06-20' })
     @IsOptional()
     @Transform(toValidDate)
     @IsDate({ message: 'La data non esiste nel calendario' })
     data?: Date;
 
+    @ApiPropertyOptional({ example: 'B3' })
     @IsOptional()
     @IsString()
     aula?: string;
 
+    @ApiPropertyOptional({ example: '14:00:00' })
     @IsOptional()
     @IsString()
     ora?: string;
 
+    @ApiPropertyOptional({ example: 'Portare calcolatrice scientifica' })
     @IsOptional()
     @IsString()
     note?: string;
 
+    @ApiPropertyOptional({ example: 2 })
     @IsOptional()
     @IsInt()
     materiaId?: number;
 
+    @ApiPropertyOptional({ example: 1 })
     @IsOptional()
     @IsInt()
     sessioneId?: number;
