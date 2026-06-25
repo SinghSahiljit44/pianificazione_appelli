ec93584c6270dda8ebb773734114acd47d6fb1a2
Matteo Lublanis <matteo.lub02@gmail.com>
2026-06-25 20:29:42 +0200

    Cambiato test

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-describe('GET /api', () => {
+describe('GET /api/data', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+    const res = await axios.get(`/api/data`);
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
